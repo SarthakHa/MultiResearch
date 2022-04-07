6260c2b78efbcd17131ecc10f7774ebad9350a15
Sarthak Harjai <sarthak@dhcp-130-58-102-198.swarthmore.edu>
2022-04-07 01:43:04 -0400

    Second Commit.

--- Self/main.py
+++ Self/main.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import numpy as np
 import torch
 from superglue import SuperGlue
-from helpers import read_image
+from helpers import read_image, make_matching_plot
 from matching import Matching
+import matplotlib.cm as cm
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
@@ -20,24 +21,24 @@ keypoint_threshold = 0.005 #default
 max_keypoints = 1024 #default
 
 ## Non-model stuff
-data_dir = 'Self_2/Self/assets/scannet_sample_images/' #This is default value
+data_dir = 'assets/scannet_sample_images/' #This is default value
 results_dir = 'dump_match_pairs/' #This is default value
 resize = [640, 480] #default
 resize_float = True #default
-matches_path = '/dump_matches/'
+matches_path = 'dump_matches'
 
 config = {
-        'superpoint': {
-            'nms_radius': nms_radius,
-            'keypoint_threshold': keypoint_threshold,
-            'max_keypoints': max_keypoints
-        },
-        'superglue': {
-            'weights': weight_to_use,
-            'sinkhorn_iterations': sinkhorn_iterations,
-            'match_threshold': match_threshold,
+            'superpoint': {
+                'nms_radius': nms_radius,
+                'keypoint_threshold': keypoint_threshold,
+                'max_keypoints': max_keypoints
+            },
+            'superglue': {
+                'weights': weight_to_use,
+                'sinkhorn_iterations': sinkhorn_iterations,
+                'match_threshold': match_threshold,
+            }
         }
-    }
 
 matching = Matching(config).eval().to(device)
 
@@ -61,9 +62,8 @@ if image0 is None or image1 is None:
 data = {'image0': inp0, 'image1': inp1} #goes into matching
 
 # Perform the matching.
-#pred = matching({'image0': inp0, 'image1': inp1})
 pred = matching(data)
-pred = {k: v[0].cpu().numpy() for k, v in pred.items()}
+pred = {k: v[0].cpu().detach().numpy() for k, v in pred.items()}
 kpts0, kpts1 = pred['keypoints0'], pred['keypoints1']
 matches, conf = pred['matches0'], pred['matching_scores0']
 
@@ -71,4 +71,40 @@ matches, conf = pred['matches0'], pred['matching_scores0']
 out_matches = {'keypoints0': kpts0, 'keypoints1': kpts1,
                 'matches': matches, 'match_confidence': conf}
 
+stem0, stem1 = Path(name0).stem, Path(name1).stem
+output_dir = matches_path
+matches_path = Path(matches_path) / '{}_{}_matches.npz'.format(stem0, stem1)
 np.savez(str(matches_path), **out_matches)
+
+# Write Viz to disk
+valid = matches > -1
+mconf = conf[valid]
+mkpts0 = kpts0[valid]
+mkpts1 = kpts1[matches[valid]]
+color = cm.jet(mconf)
+text = [
+    'SuperGlue',
+    'Keypoints: {}:{}'.format(len(kpts0), len(kpts1)),
+    'Matches: {}'.format(len(mkpts0)),
+]
+if rot0 != 0 or rot1 != 0:
+    text.append('Rotation: {}:{}'.format(rot0, rot1))
+
+# Display extra parameter info.
+k_thresh = matching.superpoint.config['keypoint_threshold']
+m_thresh = matching.superglue.config['match_threshold']
+small_text = [
+    'Keypoint Threshold: {:.4f}'.format(k_thresh),
+    'Match Threshold: {:.2f}'.format(m_thresh),
+    'Image Pair: {}:{}'.format(stem0, stem1),
+]
+
+viz_extension = 'png'
+viz_path = Path(output_dir) / '{}_{}_matches.{}'.format(stem0, stem1, viz_extension)
+show_keypoints = True
+fast_viz = False
+opencv_display = False
+make_matching_plot(
+    image0, image1, kpts0, kpts1, mkpts0, mkpts1, color,
+    text, viz_path, show_keypoints,
+    fast_viz, opencv_display, 'Matches', small_text)
